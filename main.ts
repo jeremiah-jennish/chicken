a4c5126b762572d23953022ea17efb21915a7717
jeremiah-jennish <152193898+jeremiah-jennish@users.noreply.github.com>
2023-11-27 19:08:21 +0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 player.onChat("chicken", function () {
-    for (let index = 0; index < 999999999999999; index++) {
+    for (let index = 0; index < 100; index++) {
         mobs.spawn(CHICKEN, pos(0, 10, 0))
     }
 })
